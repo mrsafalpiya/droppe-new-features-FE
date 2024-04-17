9913225d1f000a1c0ccf434f130ffe58670985b5
Safal Piya <mrsafalpiya@gmail.com>
2024-04-17 13:36:06 +0545

    Added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
     <>
       <div>
         <Button>Hehe</Button>
+        <p className='text-4xl font-bold'>Hello, World!</p>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
